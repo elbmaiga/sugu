96f857190d238dfa53688c423891039784eb9c05
bouctou04 <bmaiga04@gmail.com>
2020-12-09 17:04:14 +0000

    Rédefinition des modèles et organosation du dossier static

--- commands/admin.py
+++ commands/admin.py
@@ -2,13 +2,13 @@ from django.contrib import admin
 
 from .models import *
 # Register your models here.
-admin.site.register(Panner)
+# admin.site.register(Panner)
 
-class ClientAdmin(admin.ModelAdmin):
-    list_display = ('email', 'telephone', 'address')
-admin.site.register(Client, ClientAdmin)
+# class ClientAdmin(admin.ModelAdmin):
+#     list_display = ('email', 'telephone', 'address')
+# admin.site.register(Client, ClientAdmin)
 
-class CommandAdmin(admin.ModelAdmin):
-    list_display = ('client', 'status', 'deliverance', 'created_at')
-admin.site.register(Commands, CommandAdmin)
-admin.site.register(Articles_On_Panner)
+# class CommandAdmin(admin.ModelAdmin):
+#     list_display = ('client', 'status', 'deliverance', 'created_at')
+# admin.site.register(Commands, CommandAdmin)
+# admin.site.register(Articles_On_Panner)
